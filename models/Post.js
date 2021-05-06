633ec37a14ae22dd45ce0ce5091a0194f7c5e163
takhyunKim <kimkih1218@gmail.com>
2021-05-06 09:32:30 +0900

    Add: post Schema

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Post = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User"
+  },
+  isPublic: {
+    type: Boolean,
+    default: true
+  },
+  contents: {
+    type: String,
+    required: true
+  },
+  isAnonymous: {
+    type: String,
+    required: true
+  },
+  catagory: {
+    type: String,
+    enum: [
+      "love",
+      "course",
+      "employment",
+      "friend",
+      "pain"
+    ],
+    default: "love"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  comments: {
+    type: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Comment"
+      }
+    ],
+    default: []
+  },
+  likes: {
+    type: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    default: []
+  }
+});
+
+module.exports = mongoose.model("Post", Post);
