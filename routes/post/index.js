59f493a5a3fbed37dfa342e2c94e4469a820efec
takhyunKim <kimkih1218@gmail.com>
2021-05-07 18:05:04 +0900

    Add: get my post end point & logic

--- routes/post/index.js
+++ routes/post/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { postWorryPost } = require("../../controllers/postController");
+const {
+  postWorryPost,
+  getMyPosts
+} = require("../../controllers/postController");
 
 router.post("/", postWorryPost);
+router.get("/:userEmail", getMyPosts);
 
 module.exports = router;
