ed7212806f7d464ace2cddc2be634991d3b16278
takhyunKim <kimkih1218@gmail.com>
2021-05-14 16:43:16 +0900

    Add: socket receive, send logic for chat

--- socket/socket.js
+++ socket/socket.js
@@ -1,4 +1,6 @@
-const activatedRoomList = {};
+const ChatRoom = require("../models/ChatRoom");
+
+const activatedRoomList = {}; // { chatRoomId: { chats: [], users: [] } }
 
 module.exports = function socket(app) {
   app.io = require("socket.io")();
@@ -6,19 +8,52 @@ module.exports = function socket(app) {
   app.io.on("connect", (socket) => {
     console.log("연결 성공");
 
-    socket.on("join room", (data) => {
-      const { userEmail, friendEmail } = data;
+    socket.on("join room", async (data) => {
+      const { chatRoomId, userNickname } = data;
+      const isActivateRoom = activatedRoomList.hasOwnProperty(chatRoomId);
+
+      if (isActivateRoom) {
+        activatedRoomList[chatRoomId].users.push(userNickname);
+        socket.emit("receive inital chats", activatedRoomList[chatRoomId].chats);
+      } else {
+        const chatRoomInfo = await ChatRoom.findById(chatRoomId).lean();
+
+        activatedRoomList[chatRoomInfo._id] = {
+          chats: chatRoomInfo.comments,
+          users: [userNickname]
+        };
+
+        socket.emit("receive inital chats", activatedRoomList[chatRoomId].chats);
+      }
+    });
+
+    socket.on("leave user", async (data) => {
+      const { chatRoomId, userNickname } = data;
+      const currentRoom = activatedRoomList[chatRoomId];
+      const joinUsers = currentRoom.users;
+
+      const filteredLeaveUsers = joinUsers.filter((user) =>
+        user !== userNickname
+      );
+
+      if (!filteredLeaveUsers.length) {
+        const chatRoom = await ChatRoom.findById(chatRoomId);
+
+        await chatRoom.updateOne({
+          "$set": { comments: currentRoom.chats }
+        });
+      }
     });
 
     socket.on("send chat", (data) => {
-      const { userEmail, comment } = data;
-
+      const { userNickname, comment, chatRoomId } = data;
       const chatData = {
-        userEmail,
+        userNickname,
         comment,
         createdAt: Date.now()
       };
 
+      activatedRoomList[chatRoomId].chats.push(chatData);
       app.io.emit("receive chat", chatData);
     });
   });
